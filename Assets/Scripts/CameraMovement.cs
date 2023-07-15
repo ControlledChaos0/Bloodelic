5ed06962000e6a00a6081721a8ebb22624922e78
ControlledChaos0 <jspooner3@gatech.edu>
2023-07-14 23:26:43 -0400

    Camera Rotation cont.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraMovement : MonoBehaviour
 {
+    [SerializeField]
+    private InputActionAsset inputActions;
+    private InputActionMap cameraControls;
+    private Vector2 mouseDelta;
+    private bool moveCamera;
+
+    private void Awake() {
+        cameraControls = inputActions.FindActionMap("MainControls");
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,26 @@ public class CameraMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnEnable() {
+        
+    }
+
+    private void OnDisable() {
+        
+    }
+
+
+
+    private void OnMoveCamera(InputValue inputValue) {
+        mouseDelta = inputValue.Get<Vector2>();
+    }
+
+    private void OnCheckMoveCamera(InputValue inputValue) {
+        float checkMouse = inputValue.Get<float>();
+        if (checkMouse >= InputSystem.settings.defaultHoldTime) {
+            Debug.Log("Button Held");
+        }
     }
 }
