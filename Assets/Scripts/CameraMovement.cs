a4a91cd07352616c672db1183a10549490b6a0d9
ControlledChaos0 <jade.spoon2003@gmail.com>
2023-07-17 01:15:06 -0400

    Cam Movement Cont.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,11 +8,20 @@ public class CameraMovement : MonoBehaviour
     [SerializeField]
     private InputActionAsset inputActions;
     private InputActionMap cameraControls;
+    private InputAction moveCameraAction;
+    private InputAction checkMoveCameraAction;
     private Vector2 mouseDelta;
     private bool moveCamera;
 
     private void Awake() {
         cameraControls = inputActions.FindActionMap("MainControls");
+
+        moveCameraAction = cameraControls.FindAction("MoveCamera");
+        checkMoveCameraAction = cameraControls.FindAction("CheckMoveCamera");
+
+        checkMoveCameraAction.performed += OnCheckMoveCameraPerformed;
+        checkMoveCameraAction.canceled += OnCheckMoveCameraCanceled;
+        moveCamera = false;
     }
     // Start is called before the first frame update
     void Start()
@@ -27,23 +36,26 @@ public class CameraMovement : MonoBehaviour
     }
 
     private void OnEnable() {
-        
+        inputActions.FindActionMap("MainControls").Enable();
     }
 
     private void OnDisable() {
-        
+        inputActions.FindActionMap("MainControls").Disable();
     }
 
 
 
     private void OnMoveCamera(InputValue inputValue) {
-        mouseDelta = inputValue.Get<Vector2>();
+        mouseDelta = moveCamera ? inputValue.Get<Vector2>() : Vector2.zero;
     }
 
-    private void OnCheckMoveCamera(InputValue inputValue) {
-        float checkMouse = inputValue.Get<float>();
-        if (checkMouse >= InputSystem.settings.defaultHoldTime) {
-            Debug.Log("Button Held");
-        }
+    private void OnCheckMoveCameraPerformed(InputAction.CallbackContext context) {
+        moveCamera = true;
+        Debug.Log("Performed");
+    }
+
+    private void OnCheckMoveCameraCanceled(InputAction.CallbackContext context) {
+        moveCamera = false;
+        Debug.Log("Canceled");
     }
 }
