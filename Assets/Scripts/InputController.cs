484a407c19d70b32b8d7f89a3bc7ff202b01b78b
ControlledChaos0 <jspooner3@gatech.edu>
2023-07-30 17:30:30 -0400

    Continuing Camera Controls

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -13,7 +13,10 @@ public class InputController : MonoBehaviour
     private InputAction _moveCameraAction;
     private InputAction _checkMoveCameraAction;
     private InputAction _checkPanCameraAction;
+    private InputAction _clickAction;
     private Vector2 _mouseDelta;
+    private float _pressTime;
+    private float _clickTime;
     private bool _moveCamera;
     private bool _panCamera;
 
@@ -28,6 +31,14 @@ public class InputController : MonoBehaviour
         _checkMoveCameraAction.canceled += OnCheckMoveCameraCanceled;
         _checkPanCameraAction.performed += OnCheckPanCameraPerformed;
         _checkPanCameraAction.canceled += OnCheckPanCameraCanceled;
+        _clickAction = _cameraControls.FindAction("Click");
+
+        int holdIndex = _checkMoveCameraAction.interactions.IndexOf("Hold(duration=");
+        _pressTime = float.Parse(_checkMoveCameraAction.interactions.Substring(holdIndex + 14, 3));
+
+        _clickAction.started += OnClickStarted;
+        _clickAction.canceled += OnClickCanceled;
+
         _moveCamera = false;
         _panCamera = false;
     }
@@ -66,14 +77,24 @@ public class InputController : MonoBehaviour
         cameraController.ZoomCamera(inputValue.Get<float>());
     }
 
+    private void OnClickStarted(InputAction.CallbackContext context) {
+        _clickTime = Time.time;
+    }
+
+    private void OnClickCanceled(InputAction.CallbackContext context) {
+        if (Time.time - _clickTime < _pressTime) {
+            Debug.Log("Just a click!!!");
+        }
+    }
+
     private void OnCheckMoveCameraPerformed(InputAction.CallbackContext context) {
         _moveCamera = true;
-        Debug.Log("Performed");
+        Debug.Log("Check Move Camera Performed");
     }
 
     private void OnCheckMoveCameraCanceled(InputAction.CallbackContext context) {
         _moveCamera = false;
-        Debug.Log("Canceled");
+        Debug.Log("Check Move Camera Canceled");
     }
     private void OnCheckPanCameraPerformed(InputAction.CallbackContext context) {
         _panCamera = true;
