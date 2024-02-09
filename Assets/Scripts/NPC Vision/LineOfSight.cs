1c870312ca0fae40fa382384bc829d145c3e2c7c
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2024-02-08 23:09:35 -0500

    Extracted method for generating a visible cone.
    Not sure if this method will ultimately be used; I am debating setting up the method to instead keep the cone invisible and change the colors of blocks it can see.

--- Assets/Scripts/NPC Vision/LineOfSight.cs
+++ Assets/Scripts/NPC Vision/LineOfSight.cs
@@ -1,27 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using KevinCastejon.ConeMesh;
 
 public class LineOfSight : MonoBehaviour
 {
-    [SerializeField]
-    private const float angle = 60;
+    private const float ANGLE = 45;
+    private const int CONE_HEIGHT = 10;
 
     [SerializeField]
     private GameObject player;
+
+    [SerializeField]
+    private GameObject cone;
+
+    private Cone coneScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Debug.Log(DetectEntity(player));
     }
 
-    public bool DetectEntity(GameObject entity, float viewAngle = angle)
+    /**
+     * Method for entity detection. 
+     * Usage should normally be to detect the player, but I added
+     * the ability to detect other things.
+     * Will likely need to be integrated into a state machine
+     * @param entity - A GameObject to detect
+     * @param viewAngle - the viewing angle to check with.
+     *      should normally be the same as the angle field of this class.
+     * @return bool representing whether the item is within viewing radius.
+     */
+    public bool DetectEntity(GameObject entity, float viewAngle = ANGLE)
     {
         RaycastHit hit;
         if (Physics.Raycast(
@@ -43,4 +60,17 @@ public class LineOfSight : MonoBehaviour
         }
         return false;
     }
+
+    //TODO: Method is incomplete
+    //Will be a helper for when the npc is clicked
+    private void RevealCone()
+    {
+        coneScript = cone.GetComponent<Cone>();
+        coneScript.ConeHeight = CONE_HEIGHT;
+        coneScript.ConeRadius = ((float)(coneScript.ConeHeight * Math.Tan(ANGLE * (Math.PI / 180))));
+    }
+
+
+
+
 }
