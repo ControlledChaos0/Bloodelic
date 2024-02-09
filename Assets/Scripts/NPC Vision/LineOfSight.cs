1c063cebcb83b9189d3fae2fbc26a6f216f733fc
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2024-02-08 21:47:54 -0500

    Added ability for NPC to spot player
    Added a class called LineOfSight, which contains a method that detects the player based on a given angle.

---
+++ Assets/Scripts/NPC Vision/LineOfSight.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    [SerializeField]
+    private const float angle = 60
+
+    [SerializeField]
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+            Debug.Log(DetectEntity(player));
+    }
+
+    public bool DetectEntity(GameObject entity, float viewAngle = angle)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(
+            transform.position,
+            (entity.transform.position - transform.position).normalized,
+            out hit
+        ))
+        {
+            if (hit.transform.Equals(entity.transform))
+            {
+                Debug.Log(Vector3.Angle((entity.transform.position - transform.position).normalized, transform.forward));
+                if (Vector3.Angle(
+                    (entity.transform.position - transform.position).normalized,
+                    transform.forward) < viewAngle)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
