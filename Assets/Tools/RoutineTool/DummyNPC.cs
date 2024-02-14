0ef776d7b17c6cc81504c826efe4fd1effcc3d17
Carlos Suarez Barrera <carlijavier5@gmail.com>
2024-02-13 22:20:28 -0500

    Routine Tool GUI

--- Assets/Tools/RoutineTool/DummyNPC.cs
+++ Assets/Tools/RoutineTool/DummyNPC.cs
@@ -1,37 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+using CJUtils;
+#endif
 
 public class DummyNPC : Entity {
 
-    [SerializeField] private EntityAction[] routine;
-}
-
-/// <summary>
-/// Class to link the Routine Tool to the NPCs in the scene;
-/// <br></br> Has no runtime value; thus, it is destroyed;
-/// </summary>
-public class NPCManager : MonoBehaviour {
-    public T[] LoadEntities<T>() where T : Entity => GetComponentsInChildren<T>(true);
-    void OnEnable() => Destroy(this);
-}
-
-[CustomEditor(typeof(NPCManager))]
-public class NPCManagerEditor : Editor {
-    public override void OnInspectorGUI() {
-        NPCManager npcManager = target as NPCManager;
-        RoutineToolGUI.LaunchRoutineTool(npcManager);
+    [SerializeField] private List<EntityAction> routine;
+    public List<EntityAction> Routine {
+        get {
+            if (routine == null) routine = new();
+            return routine;
+        }
     }
 }
 
-public abstract class EntityAction {}
+public abstract class EntityAction {
+    #if UNITY_EDITOR
+    public abstract void ShowGUI();
+    #endif
+}
 
 [System.Serializable]
 public class ActionMove : EntityAction {
     public GridCell targetCell;
     public ActionMove() => targetCell = null;
     public ActionMove(GridCell targetCell) => this.targetCell = targetCell;
+
+    #if UNITY_EDITOR
+
+    bool retargeting; 
+    GridCell uncommittedCell;
+    
+    public override void ShowGUI() {
+        EditorGUIUtility.labelWidth = EditorUtils.MeasureTextWidth("Target Cell", GUI.skin.font) + 15;
+        targetCell = EditorGUILayout.ObjectField("Target Cell", uncommittedCell == null
+                                                                ? targetCell : uncommittedCell, typeof(GridCell), true) as GridCell;
+        EditorGUIUtility.labelWidth = 0;
+        if (retargeting) {
+            if (Selection.objects.Length > 0 && Selection.objects[0] is GameObject) {
+                GridCell cell = (Selection.objects[0] as GameObject).GetComponentInParent<GridCell>();
+                if (cell != null) uncommittedCell = cell;
+            }
+        }
+        if (!retargeting && targetCell == uncommittedCell) {
+            if (GUILayout.Button("Retarget Cell")) {
+                retargeting = true;
+            }
+        }
+        if (retargeting) {
+            GUI.enabled = targetCell != uncommittedCell;
+            using (new EditorGUILayout.HorizontalScope()) {
+                if (GUILayout.Button("Apply")) {
+                    targetCell = uncommittedCell;
+                    retargeting = false;
+                }
+                if (GUILayout.Button("Revert")) {
+                    uncommittedCell = targetCell;
+                    retargeting = false;
+                }
+            }
+            GUI.enabled = true;
+        }
+    }
+    #endif
 }
 
 [System.Serializable]
@@ -39,14 +73,28 @@ public class ActionWait : EntityAction {
     public int waitTime;
     public ActionWait() => waitTime = 0;
     public ActionWait(int waitTime) => this.waitTime = waitTime;
+
+    #if UNITY_EDITOR
+    public override void ShowGUI() {
+        
+    }
+    #endif
 }
 
 [System.Serializable]
 public class ActionInteract : EntityAction {
-    
+    #if UNITY_EDITOR
+    public override void ShowGUI() {
+
+    }
+    #endif
 }
 
 [System.Serializable]
 public class ActionLookAt : EntityAction {
+    #if UNITY_EDITOR
+    public override void ShowGUI() {
 
+    }
+    #endif
 }
--- Assets/Tools/RoutineTool/RoutineToolGUI.cs
+++ Assets/Tools/RoutineTool/RoutineToolGUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using CJUtils;
 
 public class RoutineToolGUI : EditorWindow {
 
@@ -15,12 +16,113 @@ public class RoutineToolGUI : EditorWindow {
     private NPCManager npcManager;
     private DummyNPC[] registeredNPCs;
 
+    private DummyNPC selectedNPC;
+    private EntityAction selectedAction;
+
     private void Init(NPCManager npcManager) {
         this.npcManager = npcManager;
         registeredNPCs = npcManager.LoadEntities<DummyNPC>();
     }
 
     void OnGUI() {
+        using (new EditorGUILayout.HorizontalScope()) {
+            DrawLeftPanel();
+        }
+    }
 
+    private void DrawLeftPanel() {
+        using (new EditorGUILayout.VerticalScope(GUILayout.Width(205))) {
+            using (new EditorGUILayout.HorizontalScope()) {
+                EditorUtils.WindowBoxLabel("NPCs", UIStyles.CenteredLabelBold, GUILayout.Height(18));
+                using (new EditorGUILayout.HorizontalScope(UIStyles.WindowBox)) {
+                    if (GUILayout.Button(EditorUtils.FetchIcon("d_Refresh"), EditorStyles.miniButton, GUILayout.Width(30))) {
+                        registeredNPCs = npcManager.LoadEntities<DummyNPC>();
+                    }
+                }
+            }
+            using (new EditorGUILayout.VerticalScope(EditorStyles.textArea)) {
+                using (new EditorGUILayout.ScrollViewScope(Vector2.zero, UIStyles.WindowBox, GUILayout.ExpandHeight(true))) {
+                    foreach (DummyNPC npc in registeredNPCs) {
+                        DrawButton(npc);
+                    }
+                }
+            }
+        }
+        using (new EditorGUILayout.VerticalScope()) {
+            EditorUtils.WindowBoxLabel(selectedNPC == null ? "No NPC Selected" : selectedNPC.gameObject.name, UIStyles.CenteredLabelBold, GUILayout.Height(18));
+            using (new EditorGUILayout.HorizontalScope()) {
+                using (new EditorGUILayout.VerticalScope(GUILayout.MinWidth(200))) {
+                    EditorUtils.WindowBoxLabel("NPC Routine", UIStyles.CenteredLabelBold);
+                    using (new EditorGUILayout.VerticalScope(EditorStyles.textArea)) {
+                        if (selectedNPC == null) {
+                            EditorUtils.DrawScopeCenteredText("Select an NPC from the List;\n" +
+                                                              "Its routine will appear here;");
+                        } else {
+                            using (new EditorGUILayout.ScrollViewScope(Vector2.zero, UIStyles.WindowBox, GUILayout.ExpandHeight(true))) {
+                                foreach (EntityAction action in selectedNPC.Routine) {
+                                    DrawButton(action);
+                                }
+                            }
+                        }
+                    }
+                }
+                using (new EditorGUILayout.VerticalScope()) {
+                    EditorUtils.WindowBoxLabel("Action Info");
+                    using (new EditorGUILayout.VerticalScope(UIStyles.WindowBox, GUILayout.ExpandHeight(true))) {
+                        if (selectedNPC == null) {
+                            EditorUtils.DrawScopeCenteredText("No NPC Selected;");
+                        } else {
+                            if (selectedAction == null) {
+                                EditorUtils.DrawScopeCenteredText("No Action Selected;");
+                            } else {
+                                selectedAction.ShowGUI();
+                                GUILayout.FlexibleSpace();
+                            }
+                        }
+                    }
+                    EditorUtils.WindowBoxLabel("Routine Controls");
+                    using (new EditorGUILayout.VerticalScope(UIStyles.WindowBox, GUILayout.ExpandHeight(true))) {
+                        if (selectedNPC == null) {
+                            EditorUtils.DrawScopeCenteredText("No NPC Selected;");
+                        } else {
+                            if (GUILayout.Button("Add Move Action")) {
+                                selectedNPC.Routine.Add(new ActionMove());
+                            }
+                            if (GUILayout.Button("Add Wait Action")) {
+                                selectedNPC.Routine.Add(new ActionWait());
+                            }
+                            if (GUILayout.Button("Add Interact Action")) {
+                                selectedNPC.Routine.Add(new ActionInteract());
+                            }
+                            if (GUILayout.Button("Add Look At Action")) {
+                                selectedNPC.Routine.Add(new ActionLookAt());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void DrawButton(DummyNPC npc) {
+        float width = EditorUtils.MeasureTextWidth(npc.gameObject.name, GUI.skin.font);
+        if (GUILayout.Button(npc.gameObject.name, selectedNPC == npc
+                                                  ? new GUIStyle(EditorStyles.numberField) {
+                                                      alignment = TextAnchor.MiddleCenter,
+                                                      normal = { textColor = UIColors.Blue } }
+                                                  : GUI.skin.button)) {
+            selectedAction = null;
+            selectedNPC = npc;
+        }
+    }
+
+    private void DrawButton(EntityAction action) {
+        if (GUILayout.Button(action.GetType().Name, selectedAction == action
+                                          ? new GUIStyle(EditorStyles.numberField) {
+                                              alignment = TextAnchor.MiddleCenter,
+                                              normal = { textColor = UIColors.Blue } }
+                                          : GUI.skin.button)) {
+            selectedAction = action;
+        }
     }
 }
