f592cc07af9a90b48572f046df7e5dd684a5ee38
Willzhang13 <yizhang@gatech.edu>
2024-02-27 01:24:39 -0500

    Initial Commit of World UI
    This is a very base version that needs revision
    - The ui menu's spawn and despawn
    - ui menu location?
    - Is it efficient rn? :/
    - WHY IS DUMMY AI NOT SELECTABLE IF IT HAS THE SCRIPT ATTACHED
    - It needs specific options implemented and the actions connect to the buttons

--- Assets/Scripts/NPC/Behaviors/Selectable.cs
+++ Assets/Scripts/NPC/Behaviors/Selectable.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class Selectable : MonoBehaviour
 {
     public Action Selection;
+    public static Action<GameObject> SelectedUI;
     public void Select(GameObject gameObject) {
+        Debug.Log(gameObject + "is selected");
         if (!this.gameObject.Equals(GameObjectHelper.GetParentGameObject(gameObject))) {
             return;
         }
         Selection.Invoke();
+        SelectedUI?.Invoke(gameObject);
     }
     // Start is called before the first frame update
     void Start()
