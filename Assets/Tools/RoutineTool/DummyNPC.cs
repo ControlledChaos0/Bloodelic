247d28796d38618a5ad6200d6ef4a9f35dad5a3a
Carlos Suarez Barrera <carlijavier5@gmail.com>
2024-03-01 01:13:11 -0500

    Tool GUI Updates

--- Assets/Tools/RoutineTool/DummyNPC.cs
+++ Assets/Tools/RoutineTool/DummyNPC.cs
@@ -1,10 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-using CJUtils;
-#endif
 
 public class DummyNPC : Entity {
 
@@ -15,86 +10,4 @@ public class DummyNPC : Entity {
             return routine;
         }
     }
-}
-
-public abstract class EntityAction {
-    #if UNITY_EDITOR
-    public abstract void ShowGUI();
-    #endif
-}
-
-[System.Serializable]
-public class ActionMove : EntityAction {
-    public GridCell targetCell;
-    public ActionMove() => targetCell = null;
-    public ActionMove(GridCell targetCell) => this.targetCell = targetCell;
-
-    #if UNITY_EDITOR
-
-    bool retargeting; 
-    GridCell uncommittedCell;
-    
-    public override void ShowGUI() {
-        EditorGUIUtility.labelWidth = EditorUtils.MeasureTextWidth("Target Cell", GUI.skin.font) + 15;
-        targetCell = EditorGUILayout.ObjectField("Target Cell", uncommittedCell == null
-                                                                ? targetCell : uncommittedCell, typeof(GridCell), true) as GridCell;
-        EditorGUIUtility.labelWidth = 0;
-        if (retargeting) {
-            if (Selection.objects.Length > 0 && Selection.objects[0] is GameObject) {
-                GridCell cell = (Selection.objects[0] as GameObject).GetComponentInParent<GridCell>();
-                if (cell != null) uncommittedCell = cell;
-            }
-        }
-        if (!retargeting && targetCell == uncommittedCell) {
-            if (GUILayout.Button("Retarget Cell")) {
-                retargeting = true;
-            }
-        }
-        if (retargeting) {
-            GUI.enabled = targetCell != uncommittedCell;
-            using (new EditorGUILayout.HorizontalScope()) {
-                if (GUILayout.Button("Apply")) {
-                    targetCell = uncommittedCell;
-                    retargeting = false;
-                }
-                if (GUILayout.Button("Revert")) {
-                    uncommittedCell = targetCell;
-                    retargeting = false;
-                }
-            }
-            GUI.enabled = true;
-        }
-    }
-    #endif
-}
-
-[System.Serializable]
-public class ActionWait : EntityAction {
-    public int waitTime;
-    public ActionWait() => waitTime = 0;
-    public ActionWait(int waitTime) => this.waitTime = waitTime;
-
-    #if UNITY_EDITOR
-    public override void ShowGUI() {
-        
-    }
-    #endif
-}
-
-[System.Serializable]
-public class ActionInteract : EntityAction {
-    #if UNITY_EDITOR
-    public override void ShowGUI() {
-
-    }
-    #endif
-}
-
-[System.Serializable]
-public class ActionLookAt : EntityAction {
-    #if UNITY_EDITOR
-    public override void ShowGUI() {
-
-    }
-    #endif
 }
--- Assets/Tools/RoutineTool/RoutineToolGUI.cs
+++ Assets/Tools/RoutineTool/RoutineToolGUI.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -7,6 +7,9 @@ using CJUtils;
 public class RoutineToolGUI : EditorWindow {
 
     public static RoutineToolGUI mainWindow;
+
+    private Vector2 npcScroll;
+    private Vector2 routineScroll;
 
     public static void LaunchRoutineTool(NPCManager npcManager) {
         mainWindow = GetWindow<RoutineToolGUI>("NPC Routine Tool");
@@ -25,13 +28,15 @@ public class RoutineToolGUI : EditorWindow {
     }
 
     void OnGUI() {
-        using (new EditorGUILayout.HorizontalScope()) {
-            DrawLeftPanel();
+        using (var scope = new EditorGUI.ChangeCheckScope()) {
+            using (new EditorGUILayout.HorizontalScope()) {
+                DrawGUI();
+            }
         }
     }
 
-    private void DrawLeftPanel() {
-        using (new EditorGUILayout.VerticalScope(GUILayout.Width(205))) {
+    private void DrawGUI() {
+        using (new EditorGUILayout.VerticalScope(GUILayout.Width(position.width * 2f/7f))) {
             using (new EditorGUILayout.HorizontalScope()) {
                 EditorUtils.WindowBoxLabel("NPCs", UIStyles.CenteredLabelBold, GUILayout.Height(18));
                 using (new EditorGUILayout.HorizontalScope(UIStyles.WindowBox)) {
@@ -41,7 +46,8 @@ public class RoutineToolGUI : EditorWindow {
                 }
             }
             using (new EditorGUILayout.VerticalScope(EditorStyles.textArea)) {
-                using (new EditorGUILayout.ScrollViewScope(Vector2.zero, UIStyles.WindowBox, GUILayout.ExpandHeight(true))) {
+                using (var scope = new EditorGUILayout.ScrollViewScope(npcScroll, UIStyles.WindowBox, GUILayout.ExpandHeight(true))) {
+                    npcScroll = scope.scrollPosition;
                     foreach (DummyNPC npc in registeredNPCs) {
                         DrawButton(npc);
                     }
@@ -51,16 +57,21 @@ public class RoutineToolGUI : EditorWindow {
         using (new EditorGUILayout.VerticalScope()) {
             EditorUtils.WindowBoxLabel(selectedNPC == null ? "No NPC Selected" : selectedNPC.gameObject.name, UIStyles.CenteredLabelBold, GUILayout.Height(18));
             using (new EditorGUILayout.HorizontalScope()) {
-                using (new EditorGUILayout.VerticalScope(GUILayout.MinWidth(200))) {
+                using (new EditorGUILayout.VerticalScope(GUILayout.MinWidth(position.width * 2f / 7f))) {
                     EditorUtils.WindowBoxLabel("NPC Routine", UIStyles.CenteredLabelBold);
                     using (new EditorGUILayout.VerticalScope(EditorStyles.textArea)) {
                         if (selectedNPC == null) {
                             EditorUtils.DrawScopeCenteredText("Select an NPC from the List;\n" +
                                                               "Its routine will appear here;");
                         } else {
-                            using (new EditorGUILayout.ScrollViewScope(Vector2.zero, UIStyles.WindowBox, GUILayout.ExpandHeight(true))) {
-                                foreach (EntityAction action in selectedNPC.Routine) {
-                                    DrawButton(action);
+                            using (var scope = new EditorGUILayout.ScrollViewScope(routineScroll, UIStyles.WindowBox, GUILayout.ExpandHeight(true))) {
+                                routineScroll = scope.scrollPosition;
+                                if (selectedNPC.Routine.Count == 0) {
+                                    EditorUtils.DrawScopeCenteredText("- Empty -");
+                                } else {
+                                    for (int i = 0; i < selectedNPC.Routine.Count; i++) {
+                                        DrawButton(i, selectedNPC.Routine[i]);
+                                    }
                                 }
                             }
                         }
@@ -91,12 +102,14 @@ public class RoutineToolGUI : EditorWindow {
                             if (GUILayout.Button("Add Wait Action")) {
                                 selectedNPC.Routine.Add(new ActionWait());
                             }
+                            GUI.enabled = false; /// Not Implemented!
                             if (GUILayout.Button("Add Interact Action")) {
                                 selectedNPC.Routine.Add(new ActionInteract());
                             }
                             if (GUILayout.Button("Add Look At Action")) {
                                 selectedNPC.Routine.Add(new ActionLookAt());
                             }
+                            GUI.enabled = true;
                         }
                     }
                 }
@@ -105,24 +118,40 @@ public class RoutineToolGUI : EditorWindow {
     }
 
     private void DrawButton(DummyNPC npc) {
-        float width = EditorUtils.MeasureTextWidth(npc.gameObject.name, GUI.skin.font);
         if (GUILayout.Button(npc.gameObject.name, selectedNPC == npc
-                                                  ? new GUIStyle(EditorStyles.numberField) {
-                                                      alignment = TextAnchor.MiddleCenter,
-                                                      normal = { textColor = UIColors.Blue } }
-                                                  : GUI.skin.button)) {
+                                        ? new GUIStyle(EditorStyles.numberField) {
+                                            alignment = TextAnchor.MiddleCenter,
+                                            normal = { textColor = UIColors.Blue } }
+                                        : GUI.skin.button)) {
             selectedAction = null;
             selectedNPC = npc;
         }
     }
 
-    private void DrawButton(EntityAction action) {
-        if (GUILayout.Button(action.GetType().Name, selectedAction == action
-                                          ? new GUIStyle(EditorStyles.numberField) {
-                                              alignment = TextAnchor.MiddleCenter,
-                                              normal = { textColor = UIColors.Blue } }
-                                          : GUI.skin.button)) {
-            selectedAction = action;
+    private void DrawButton(int index, EntityAction action) {
+        using (new EditorGUILayout.HorizontalScope()) {
+            GUI.color = UIColors.Red;
+            if (GUILayout.Button(EditorUtils.FetchIcon("d_TreeEditor.Trash"), GUILayout.Width(32))) {
+                if (selectedAction == action) selectedAction = null;
+                selectedNPC.Routine.RemoveAt(index);
+            } GUI.color = Color.white;
+            if (GUILayout.Button(action.GetType().Name, selectedAction == action
+                                              ? new GUIStyle(EditorStyles.numberField) {
+                                                  alignment = TextAnchor.MiddleCenter,
+                                                  normal = { textColor = UIColors.Blue } }
+                                              : GUI.skin.button)) {
+                selectedAction = action;
+            }
+            if (index == 0) GUI.enabled = false;
+            if (GUILayout.Button("▲", GUILayout.Width(24))) {
+                selectedNPC.Routine.RemoveAt(index);
+                selectedNPC.Routine.Insert(index - 1, action);
+            } GUI.enabled = true;
+            if (index == selectedNPC.Routine.Count - 1) GUI.enabled = false;
+            if (GUILayout.Button("▼", GUILayout.Width(24))) {
+                selectedNPC.Routine.RemoveAt(index);
+                selectedNPC.Routine.Insert(index + 1, action);
+            } GUI.enabled = true;
         }
     }
 }
